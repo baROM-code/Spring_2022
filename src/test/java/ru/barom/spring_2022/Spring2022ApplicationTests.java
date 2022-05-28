5125a3a85444483f966c88cdfc7836b5768ba0d7
Roman Bar <barom64@mail.ru>
2022-05-28 09:35:29 +0400

    Initial commit

---
+++ src/test/java/ru/barom/spring_2022/Spring2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.barom.spring_2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring2022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
